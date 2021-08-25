f79140f5516cb04306ccfeebcaeaac4068268b7b
Tatiana Muniz <tati.muniz0@gmail.com>
2021-08-25 14:12:34 +0300

    Feature: gameScreen and MainMenuScreen classes

--- ios/src/com/mygdx/game/IOSLauncher.java
+++ ios/src/com/mygdx/game/IOSLauncher.java
@@ -5,13 +5,12 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.mygdx.game.Game;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Game(), config);
+        return new IOSApplication(new Gaame(), config);
     }
 
     public static void main(String[] argv) {
